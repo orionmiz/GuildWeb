f506cbd2c6d6651934efcbd5c472fc398df6931d
orionmiz <39466936+orionmiz@users.noreply.github.com>
2021-07-26 14:37:33 +0900

    Docs: deploy prototype and add README (#1)
    * Added custom authentication feature
    
    * Build: add prototype files for github pages
    
    * Readme: add readme and preview images
    
    * Readme: add url of running instance

--- src/main/java/com/orionmiz/guildweb/security/SecurityConfig.java
+++ src/main/java/com/orionmiz/guildweb/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.orionmiz.guildweb.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,9 +13,13 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     final SecurityMembersService service;
+    final CustomAuthenticationProvider authenticationProvider;
+    final CustomAuthenticationFailureHandler failureHandler;
 
-    public SecurityConfig(SecurityMembersService service) {
+    public SecurityConfig(SecurityMembersService service, CustomAuthenticationProvider provider, CustomAuthenticationFailureHandler failureHandler) {
         this.service = service;
+        this.authenticationProvider = provider;
+        this.failureHandler = failureHandler;
     }
 
     @Override
@@ -21,11 +28,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/home").permitAll()
                 .antMatchers("/boards/**").permitAll();
 
-        http.formLogin().loginPage("/login");
+        http.formLogin().loginPage("/login").failureHandler(failureHandler);
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(service).passwordEncoder(NoOpPasswordEncoder.getInstance());
+        //auth.userDetailsService(service).passwordEncoder(NoOpPasswordEncoder.getInstance());
+        auth.authenticationProvider(authenticationProvider);
     }
 }
--- src/main/java/com/orionmiz/guildweb/security/SecurityMember.java
+++ src/main/java/com/orionmiz/guildweb/security/SecurityMember.java
@@ -2,6 +2,8 @@ package com.orionmiz.guildweb.security;
 
 import com.orionmiz.guildweb.domain.Member;
 import com.orionmiz.guildweb.domain.MemberRole;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -10,17 +12,23 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+
+@Getter
+@Setter
 public class SecurityMember extends User {
 
+    private boolean enabled;
+
     public SecurityMember(Member member) {
-        super(member.getUsername(), member.getPassword(), toGrantedAuthorities(member.getRoles()));
+        super(member.getUsername(), member.getPassword(), makeGrantedAuthorities(member.getRoles()));
+        this.enabled = member.isEnabled();
     }
 
     public SecurityMember(String username, String password, Collection<? extends GrantedAuthority> authorities) {
         super(username, password, authorities);
     }
 
-    private static List<SimpleGrantedAuthority> toGrantedAuthorities(List<MemberRole> roles) {
+    private static List<SimpleGrantedAuthority> makeGrantedAuthorities(List<MemberRole> roles) {
         List<SimpleGrantedAuthority> list = new ArrayList<>();
         roles.forEach( role -> { list.add(new SimpleGrantedAuthority(role.getRoleName())); } );
         return list;
